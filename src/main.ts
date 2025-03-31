085b239764073932171a0a761212d19d085cc7e6
Samson Alajede <ajesamson@yahoo.com>
2025-03-31 18:21:55 +0100

    Add swagger configuration for endpoint documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Music Booking')
+    .setDescription('A music booking api implementation')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      jsonDocumentUrl: '/api/swagger.json',
+    },
+  });
+
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
