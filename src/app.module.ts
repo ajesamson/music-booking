22b17e1517070dfd6e3ecaab7d7b1655aa5606f6
Samson Alajede <ajesamson@yahoo.com>
2025-03-31 18:21:26 +0100

    Added PrismaModule and ArtistsModule to the entry point

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ArtistsModule } from './artists/artists.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, ArtistsModule],
   controllers: [AppController],
   providers: [AppService],
 })
