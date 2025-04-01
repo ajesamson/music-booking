6dc8b3fd1c342c874583246935c7e85bf6cc7c74
Samson Alajede <ajesamson@yahoo.com>
2025-04-01 13:09:10 +0100

    Added create artist dto for new artist

--- src/artists/dto/create-artist.dto.ts
+++ src/artists/dto/create-artist.dto.ts
@@ -1,1 +1,50 @@
-export class CreateArtistDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateArtistDto {
+  @IsString()
+  @MaxLength(45)
+  @MinLength(1)
+  @ApiProperty()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ required: false })
+  imageUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ required: false })
+  genre?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ required: false })
+  bio?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ required: false })
+  instagramUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ required: false })
+  facebookUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ required: false })
+  twitterUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ required: false })
+  spotifyUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  @ApiProperty({ required: false })
+  websiteUrl?: string;
+}
