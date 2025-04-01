6db8bee6fcff9ee6c6b9f8ce332a50f2be274877
Samson Alajede <ajesamson@yahoo.com>
2025-04-01 13:14:51 +0100

    Added implementation for artist controller endpoints

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -1,34 +1,58 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ArtistsService } from './artists.service';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ArtistResponseDto } from './dto/artist-response.dto';
+import { ApiResponse } from 'src/dto/api-response.dto';
 
 @Controller('artists')
+@ApiTags('artists')
 export class ArtistsController {
   constructor(private readonly artistsService: ArtistsService) {}
 
   @Post()
-  create(@Body() createArtistDto: CreateArtistDto) {
-    return this.artistsService.create(createArtistDto);
+  @ApiCreatedResponse({ type: ArtistResponseDto })
+  async create(
+    @Body() createArtistDto: CreateArtistDto,
+  ): Promise<ApiResponse<ArtistResponseDto>> {
+    return await this.artistsService.create(createArtistDto);
   }
 
   @Get()
-  findAll() {
-    return this.artistsService.findAll();
+  @ApiOkResponse({ type: ArtistResponseDto, isArray: true })
+  async findAll(): Promise<ApiResponse<ArtistResponseDto[]>> {
+    return await this.artistsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.artistsService.findOne(+id);
+  @ApiOkResponse({ type: ArtistResponseDto })
+  async findOne(
+    @Param('id') id: string,
+  ): Promise<ApiResponse<ArtistResponseDto>> {
+    return this.artistsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateArtistDto: UpdateArtistDto) {
-    return this.artistsService.update(+id, updateArtistDto);
+  @ApiOkResponse({ type: ArtistResponseDto })
+  async update(
+    @Param('id') id: string,
+    @Body() updateArtistDto: UpdateArtistDto,
+  ): Promise<ApiResponse<ArtistResponseDto>> {
+    return this.artistsService.update(id, updateArtistDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.artistsService.remove(+id);
+  @ApiOkResponse({ type: ArtistResponseDto })
+  remove(@Param('id') id: string): Promise<ApiResponse<ArtistResponseDto>> {
+    return this.artistsService.remove(id);
   }
 }
